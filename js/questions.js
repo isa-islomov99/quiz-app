341f67352d9cfe7875abbd51b9eeea210165a5ee
isa-islomov99 <example@gmail.com>
2021-06-13 14:31:56 +0300

    deploy

---
+++ js/questions.js
@@ -0,0 +1,365 @@
+const questions = [
+    {
+        numb: 1,
+        question: "1. Payg'ambarimiz Sollaloxu Alayxi Vasallamning Onalari nechchi yoshliklarida vafot etganlar ?",
+        answer: "6 yoshliklarida",
+        options: [
+            "4 yoshliklarida",
+            "5 yoshliklarida",
+            "6 yoshliklarida",
+            "7 yoshliklarida",
+        ]
+    },
+    {
+        numb: 2,
+        question: "2. Payg'ambarimiz Sollaloxu Alayxi Vasallam Xadicha Onamizdan nechta farzand ko'rganlar ?",
+        answer: "7 ta",
+        options: [
+            "7 ta",
+            "5 ta",
+            "6 ta",
+            "4 ta",
+        ]
+    },
+    {
+        numb: 3,
+        question: "3. Badr jangi nechanchi yilda sodir bo'lgan ?",
+        answer: "624-yil",
+        options: [
+            "621-yil",
+            "624-yil",
+            "625-yil",
+            "626-yil",
+        ]
+    },
+    {
+        numb: 4,
+        question: "4. Payg‘ambarimiz Sollaloxu Alayxi Vasallam ning hijratlari nechchi yoshlarida sodir bo'lgan ?",
+        answer: "53 yoshlarida",
+        options: [
+            "51 yoshlarida",
+            "53 yoshlarida",
+            "54 yoshlarida",
+            "56 yoshlarida",
+        ]
+    },
+    {
+        numb: 5,
+        question: "5. Payg‘ambarimiz Sollaloxu Alayxi Vasallam nechchi yoshliklarida Bobolari Abdulmutallib vafot qilganlar ?",
+        answer: "8 yoshlarida",
+        options: [
+            "7 yoshlarida",
+            "9 yoshlarida",
+            "10 yoshlarida",
+            "8 yoshlarida",
+        ]
+    },
+    {
+        numb: 6,
+        question: "6. Maryam onamiz Yaxho Alayhissalomga kim bo'lgan ?",
+        answer: "Xolalari",
+        options: [
+            "Xolalari",
+            "Ammalari",
+            "Opalari",
+            "Begona",
+        ]
+    },
+    {
+        numb: 7,
+        question: "7. Tushida 11ta yulduz quyosh va oyni o'ziga sajda qilayotganini ko'rgan payg'ambar qaysi ?",
+        answer: "Yusuf Alayhissalom",
+        options: [
+            "Ibrohim Alayhissalom",
+            "Dovud Alayhissalom",
+            "Yunus Alayhissalom",
+            "Yusuf Alayhissalom",
+        ]
+    },
+    {
+        numb: 8,
+        question: "8. Birinchi bo'lib azon aytgan Saxobiy ismlari ?",
+        answer: "Bilol R.A",
+        options: [
+            "Ibn Muoz R.A",
+            "Bilol R.A",
+            "Umar R.A",
+            "Usmon R.A",
+        ]
+    },
+    {
+        numb: 9,
+        question: "9. Quyidagilar orasidan qaysi Ayolni ismlari Qu'ron da zikri kelgan ?",
+        answer: "Maryam onamiz",
+        options: [
+            "Xadicha onamiz",
+            "Maryam onamiz",
+            "Oisha onamiz",
+            "Osiyo onamiz",
+        ]
+    },
+    {
+        numb: 10,
+        question: "10. Payg‘ambarimiz Sollaloxu Alayxi Vasallamning ko'ksilari yorilish voqesi nima deb ataladi ?",
+        answer: "Sharhi sadr",
+        options: [
+            "Fath alqalb",
+            "Sharxi qadr",
+            "Sharhi sadr",
+            "Hadath alqalb",
+        ]
+    },
+    {
+        numb: 11,
+        question: "11. Oyatal kursi qaysi surada kelgan ?",
+        answer: "Baqara",
+        options: [
+            "Tavba",
+            "Moida",
+            "Oli Imron",
+            "Baqara",
+        ]
+    },
+    {
+        numb: 12,
+        question: "12. Asadulloh (Allohning arsloni) nomini olgan sahoba kim edi ?",
+        answer: "Hamza Ibn Abdumuttalib R.A",
+        options: [
+            "Abu Bakr Siddiq R.A",
+            "Xolid Ibn Valid R.A",
+            "Hamza Ibn Abdumuttalib R.A",
+            "Umar Ibn Hattob R.A",
+        ]
+    },
+    {
+        numb: 13,
+        question: "13. Qur'ondagi eng kichik sura ?",
+        answer: "Kavsar surasi",
+        options: [
+            "Ixlos surasi",
+            "Kavsar surasi",
+            "Asr surasi",
+            "Nasr surasi",
+        ]
+    },
+    {
+        numb: 14,
+        question: "14. Alloh Taolo qaysi payg'ambariga qushlar va jamiyki hayvonlar tilini tushunish fazlini berdi ?",
+        answer: "Sulaymon Alayhissalomga",
+        options: [
+            "Sulaymon Alayhissalomga",
+            "Nux Alayhissalomga",
+            "Yunus Alayhissalomga",
+            "Ibrohim Alayhissalomga",
+        ]
+    },
+    {
+        numb: 15,
+        question: "15. Payg‘ambarimiz Sollaloxu Alayxi Vasallam Madinaga haftaning qaysi kuni kirib borganlar ?",
+        answer: "Juma",
+        options: [
+            "Shanba",
+            "Juma",
+            "Chorshanba",
+            "Dushanba",
+        ]
+    },
+    {
+        numb: 16,
+        question: "16. Umar R.A ning hilifaliklari qancha vaqt davom etgan ?",
+        answer: "10 yilu 6 oy",
+        options: [
+            "10 yilu 4 oy",
+            "10 yilu 7 oy",
+            "10 yilu 6 oy",
+            "10 yilu 2 oy",
+        ]
+    },
+    {
+        numb: 17,
+        question: "17. Qur'oni Karimda ismi zikr qilingan Sahoba ismlari ?",
+        answer: "Zayd Ibn Xorisa R.A",
+        options: [
+            "Zayd Ibn Xorisa R.A",
+            "Umar Ibn Hattob R.A",
+            "Abu Bakr Siddiq R.A",
+            "Ali Ibn Abu Tolib R.A",
+        ]
+    },
+    {
+        numb: 18,
+        question: "18. Al-Jome As-Sahih kitobining muallifi kim ?",
+        answer: "Imom Buxoriy",
+        options: [
+            "Imom Termiziy",
+            "Imom Abu Dovud",
+            "Imom Buxoriy",
+            "Imom Muslim",
+        ]
+    },
+    {
+        numb: 19,
+        question: "19. Islom Tarixida eng yosh islomni qabul qilgan kishi ?",
+        answer: "Ali Ibn Abu Tolib R.A",
+        options: [
+            "Usmon Ibn Affon R.A",
+            "Ali Ibn Abu Tolib R.A",
+            "Abu Zar R.A",
+            "Abu Dardo R.A",
+        ]
+    },
+    {
+        numb: 20,
+        question: "20. Payg‘ambarimiz Sollaloxu Alayxi Vasallamdan keyin ummat orasida 1-Jannatga kiradigan inson ?",
+        answer: "Abu Bakr Siddiq R.A",
+        options: [
+            "Umar Ibn Hattob R.A",
+            "Ali Ibn Abu Tolib R.A",
+            "Abu Bakr Siddiq R.A",
+            "Usmon Ibn Affon R.A",
+        ]
+    },
+    {
+        numb: 21,
+        question: "21. Payg‘ambarimiz Sollaloxu Alayxi Vasallamning Bobolari Abdulmutallib necha yoshlarida vafot qilganlar ?",
+        answer: "82 yoshlarida",
+        options: [
+            "78 yoshlarida",
+            "75 yoshlarida",
+            "82 yoshlarida",
+            "80 yoshlarida",
+        ]
+    },
+    {
+        numb: 22,
+        question: "22. Qaysi namozning 2 rakat sunnati dunyo va undagi barcha narsalardan yaxshiroq ?",
+        answer: "Bomdod Namozining",
+        options: [
+            "Shom Namozining",
+            "Bomdod Namozining",
+            "Asr Namozining",
+            "Xufton Namozining",
+        ]
+    },
+    {
+        numb: 23,
+        question: "23. Otalari but yasovchi bo'lgan Payg‘ambar ismlari ?",
+        answer: "Ibrohim Alayhissalom",
+        options: [
+            "Sulaymon Alayhissalom",
+            "Nux Alayhissalom",
+            "Yunus Alayhissalom",
+            "Ibrohim Alayhissalom",
+        ]
+    },
+    {
+        numb: 24,
+        question: "24. Taxoratda nechta farz bor ?",
+        answer: "4ta",
+        options: [
+            "5ta",
+            "3ta",
+            "4ta",
+            "6ta",
+        ]
+    },
+    {
+        numb: 25,
+        question: "25. G'uslda nechta farz bor ?",
+        answer: "3ta",
+        options: [
+            "4ta",
+            "3ta",
+            "5ta",
+            "6ta",
+        ]
+    },
+    {
+        numb: 26,
+        question: "26. Xalifalar ichida 1- bo'lib shaxid bo'lgan Xalifa kim ?",
+        answer: "Umar Ibn Hattob R.A",
+        options: [
+            "Ali Ibn Abu Tolib R.A",
+            "Umar Ibn Hattob R.A",
+            "Abu Bakr Siddiq R.A",
+            "Usmon Ibn Affon R.A",
+        ]
+    },
+    {
+        numb: 27,
+        question: "27. Fir'avnning ayoli, Jannat saidalaridan bo'lgan ayolning ismi ?",
+        answer: "Osiyo",
+        options: [
+            "MAryam",
+            "Sora",
+            "Osiyo",
+            "Xojar",
+        ]
+    },
+    {
+        numb: 28,
+        question: "28. Abu Turob (tuproqqa belangan) nomi qaysi Sahobaga berilgan edi ?",
+        answer: "Umar Ibn Hattob R.A",
+        options: [
+            "Ali Ibn Abu Tolib R.A",
+            "Umar Ibn Hattob R.A",
+            "Abu Bakr Siddiq R.A",
+            "Usmon Ibn Affon R.A",
+        ]
+    },
+    {
+        numb: 29,
+        question: "29. Payg‘ambarimiz Sollaloxu Alayxi Vasallamning Otalari ismi ?",
+        answer: "Abdulloh",
+        options: [
+            "Abdulmuttalib",
+            "Abu Tolib",
+            "Hamza",
+            "Abdulloh",
+        ]
+    },
+    {
+        numb: 30,
+        question: "30. Beshikda yotgan vaqtidayoq gapirgan Payg‘ambar ?",
+        answer: "Iso Alayhissalom",
+        options: [
+            "Yusuf Alayhissalom",
+            "Muso Alayhissalom",
+            "Iso Alayhissalom",
+            "Ibrohim Alayhissalom",
+        ]
+    },
+    {
+        numb: 31,
+        question: "31. Iso Alayhissalomga nozil bo'lgan kitob ?",
+        answer: "Injil",
+        options: [
+            "Tavrot",
+            "Injil",
+            "Zabur",
+            "Qur'on",
+        ]
+    },
+    {
+        numb: 32,
+        question: "32. Islomda aza necha kun ?",
+        answer: "3 kun",
+        options: [
+            "3 kun",
+            "7 kun",
+            "5 kun",
+            "30 kun",
+        ]
+    },
+    {
+        numb: 33,
+        question: "33. Juma kuni qaysi surani o'qish tavsiya qilinadi ?",
+        answer: "Kahf surasi",
+        options: [
+            "Ixlos surasi",
+            "Mulk surasi",
+            "Kahf surasi",
+            "Yasin surasi",
+        ]
+    },
+]
---
+++ js/script.js
@@ -0,0 +1,248 @@
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".quit");
+const continue_btn = info_box.querySelector(".restart");
+const quiz_box = document.querySelector(".quiz_box");
+const next_btn = quiz_box.querySelector(".next_btn");
+const timer_sec = quiz_box.querySelector(".timer");
+const time_line = quiz_box.querySelector(".time_line");
+const result_box = document.querySelector(".result_box");
+const quitBtn = result_box.querySelector(".quit");
+const user_loose_box = document.querySelector(".user_loose_box");
+const loose_btn = user_loose_box.querySelector(".loose_btn");
+const surprise_box = document.querySelector(".surprise_box");
+const finish_btn = surprise_box.querySelector(".finish_btn");
+
+const option_list = quiz_box.querySelector(".option_list");
+
+start_btn.onclick = () => {
+    info_box.classList.add("activeInfo")
+    start_btn.style.display = "none";
+}
+
+exit_btn.onclick = () => {
+    info_box.classList.remove("activeInfo")
+    start_btn.style.display = "block";
+}
+
+continue_btn.onclick = () => {
+    info_box.classList.remove("activeInfo")
+    start_btn.style.display = "none";
+    quiz_box.classList.add("activeQuiz")
+    showQuestions(que_count);
+    queCounter(bott_que_counter);
+    timerQuestion(20);
+    timeLine(timeLineWidth);
+}
+
+quitBtn.onclick = () => {
+    result_box.classList.remove("activeResult")
+    surprise_box.classList.add("activeSurprise")
+}
+
+loose_btn.onclick = () => {
+    window.location.reload();
+}
+finish_btn.onclick = () => {
+    window.location.reload();
+}
+
+let que_count = 0;
+let bott_que_counter = 1;
+let countCorrAnsw = 0;
+let countTime;
+let valueCountTime = 21;
+let timeLineWidth = 0;
+let countLine;
+let numbCorrectAnswer = 0;
+let numbWrongAnswer = 0;
+let countUserScore = 0;
+
+const showQuestions = (index) => {
+    const que_text = document.querySelector(".que_text");
+    const que_tag = '<span>' + questions[index].question + '</span>';
+    const option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+                    +'<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+                    +'<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+                    +'<div class="option">'+ questions[index].options[3] +'<span></span></div>'
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+    const option = option_list.querySelectorAll(".option");
+    for(let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+next_btn.onclick = () => {
+    if (que_count < questions.length - 1) {
+        que_count++;
+        showQuestions(que_count);
+        bott_que_counter++;
+        queCounter(bott_que_counter);
+        next_btn.classList.remove("show");
+        clearInterval(countTime)
+        timerQuestion(valueCountTime);
+        timeLine(timeLineWidth);
+    }else {
+        correctAnswers();
+        console.log("oshib ketti");
+    }
+}
+
+let iconTick = `<div class="icon tick"><i class="fas fa-check"></i></div>`
+let iconCross = `<div class="icon cross"><i class="fas fa-times"></i></div>`
+
+function optionSelected (answer) {
+    clearInterval(countTime)
+    clearInterval(countLine)
+    next_btn.classList.add("show");
+    let usAns = answer.textContent;
+    let correctAns = questions[que_count].answer
+    const allOptions = option_list.children.length
+    if(usAns == correctAns) {
+        answer.classList.add("correct")
+        answer.insertAdjacentHTML('beforeend', iconTick)
+        numbCorrectAnswer++;
+        countUserScore += 2;
+        var count = 200;
+        var defaults = {
+        origin: { y: 0.7 }
+        };
+
+        function fire(particleRatio, opts) {
+        confetti(Object.assign({}, defaults, opts, {
+            particleCount: Math.floor(count * particleRatio)
+        }));
+        }
+
+        fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
+        });
+        fire(0.2, {
+        spread: 60,
+        });
+        fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8
+        });
+        fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2
+        });
+        fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+        });
+    }else {
+        numbWrongAnswer++
+        countUserScore -= 2;
+        answer.classList.add("incorrect")
+        answer.insertAdjacentHTML('beforeend', iconCross)
+
+        if(numbWrongAnswer == 3 ) {
+            document.body.style.background = "rgba(108, 0, 0, 0.78)"
+            user_loose_box.classList.add("activeLoose")
+            info_box.classList.remove("activeInfo")
+            quiz_box.classList.remove("activeQuiz")
+            result_box.classList.remove("activeResult")
+            start_btn.style.display = "none";
+        }
+        
+        for (let x = 0; x < allOptions; x++) {
+            if(option_list.children[x].textContent == correctAns) {
+                option_list.children[x].setAttribute("class", "option correct");
+                option_list.children[x].insertAdjacentHTML('beforeend', iconTick)
+            }
+        }
+    }
+
+    for (let x = 0; x < allOptions; x++) {
+        option_list.children[x].classList.add("disabled")
+    }
+}
+
+function timerQuestion (time) {
+    countTime = setInterval(startTimer, 1000)
+    function startTimer() {
+        time--;
+        timer_sec.textContent = time
+        if(time < 20) {
+            timer_sec.textContent = `0:${time}`
+        }
+        if(time < 10) {
+            timer_sec.textContent = `0:0${time}`
+        }
+        if(time < 0) {
+            timer_sec.textContent = "00";
+            clearInterval(countTime)
+            document.body.style.background = "rgba(108, 0, 0, 0.78)"
+            user_loose_box.classList.add("activeLoose")
+            info_box.classList.remove("activeInfo")
+            quiz_box.classList.remove("activeQuiz")
+            result_box.classList.remove("activeResult")
+        }
+    }
+}
+
+function timeLine (count) {
+    countLine = setInterval(startTimeLine, 33)
+    function startTimeLine() {
+        count += 1
+        time_line.style.width = `${count}px`;
+        if(count > 613) {
+            clearInterval(countLine)
+        }
+    }
+}
+
+function correctAnswers() {
+    var end = Date.now() + (15 * 1000);
+    var colors = ['#bb0000', '#ffffff'];
+    (function frame() {
+    confetti({
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 },
+        colors: colors
+    });
+    confetti({
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1 },
+        colors: colors
+    });
+
+    if (Date.now() < end) {
+        requestAnimationFrame(frame);
+    }
+    }());
+    info_box.classList.remove("activeInfo")
+    quiz_box.classList.remove("activeQuiz")
+    result_box.classList.add("activeResult")
+    const complete_text = result_box.querySelector(".complete_text");
+    if(numbCorrectAnswer > 2 ) {
+        let textAnsCount = `<span><p>Siz ${questions.length} ta savoldan ${numbCorrectAnswer} tasiga to'g'ri javob bergan holda ${countUserScore} ball qo'lga kiritib Viktorinamizni mutloq g'olibiga aylandingiz. Ilmda bardavom bo'ling aziz dindoshim!</p></span>`;
+        complete_text.innerHTML = textAnsCount;
+    }
+}
+
+function queCounter (index) {
+    const que_counter = quiz_box.querySelector(".total_que");
+    const header_que_count = quiz_box.querySelector(".header_que_count");
+    const userScoreText = quiz_box.querySelector(".title");
+    let headScoreCount = `<button>${countUserScore}ball</button>`;
+    userScoreText.innerHTML = headScoreCount;
+    let headQueCount = `<button>${index}-savol</button>`;
+    header_que_count.innerHTML = headQueCount;
+    let txtQueCount = `<span><p>${index}</p> / <p>${questions.length}</p>Savollar</span>`;
+    que_counter.innerHTML = txtQueCount;
+}
+
+
+
